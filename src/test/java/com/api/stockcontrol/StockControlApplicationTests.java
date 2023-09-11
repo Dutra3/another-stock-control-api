24dce61e9a0ce0e5c160fbed1f56fedab52a4064
Dutra3 <gabrieldutra45@gmail.com>
2023-09-10 23:43:32 -0300

    starting stock control api

---
+++ src/test/java/com/api/stockcontrol/StockControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.stockcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
